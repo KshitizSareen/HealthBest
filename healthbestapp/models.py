06dbac3a65b324c799175e0b535886775d8dad5a
Kshitiz Sareen <kshitizsareen709@gmail.com>
2020-09-15 22:22:43 +0300

    new commit

--- healthbestapp/models.py
+++ healthbestapp/models.py
@@ -4,4 +4,4 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Diseases(models.Model):
     title=models.CharField(max_length=100)
-    symptoms=ArrayField(models.CharField(max_length=200),blank=True)
+    symptoms=models.TextField()
